3a479d82652f9856155df4d9fe9ad9240b8409ce
Finn Thye <piqueen@gmail.com>
2019-08-15 16:26:08 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,23 +8,23 @@ end
 
 def display_card_total(total)
   puts "Your cards add up to #{total}"
-  
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  total = deal_card + deal_card
+  display_card_total(total)
 end
 
 def hit?
